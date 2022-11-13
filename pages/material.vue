380fddc5a5db8a93219ecbad04532b8fd8340317
Dany Sluijk <me@dany.dev>
2022-11-13 21:41:03 +0100

    feat: add simple menu to the sidebar

--- pages/material.vue
+++ pages/material.vue
@@ -1,8 +1,16 @@
 <template>
   <TwoColumn title="Materiaal">
     <template #content>
-      <NuxtLink to="/material">Materiaal Home</NuxtLink>
-      <NuxtLink to="/material/types">Materiaaltypes</NuxtLink>
+      <ul class="menu rounded-box">
+        <li class="menu-title">
+          <span>Category</span>
+        </li>
+        <li><NuxtLink to="/material">Materiaal Home</NuxtLink></li>
+        <li class="menu-title">
+          <span>Category</span>
+        </li>
+        <li><NuxtLink to="/material/types">Materiaaltypes</NuxtLink></li>
+      </ul>
     </template>
     <NuxtPage />
   </TwoColumn>
