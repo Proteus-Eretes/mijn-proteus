bb7f6a341c296670663ea771616f984b4ea66815
Gerben Mulder <gerbenmulder2508@gmail.com>
2022-11-14 20:12:22 +0100

    fix: Group seed imports

--- server/prisma/seed/group.ts
+++ server/prisma/seed/group.ts
@@ -1,7 +1,6 @@
 import {
   array,
   assert,
-  date,
   Describe,
   lazy,
   object,
@@ -9,60 +8,51 @@ import {
   size,
   string,
 } from "superstruct";
-
-import { Group, Prisma } from "@prisma/client";
+import { Group } from "@prisma/client";
+import { dateString } from "../../validation";
+import { prisma } from "../../prisma/client";
 import groups from "./testdata/group.json" assert { type: "json" };
-import { group } from "~/server/logic";
-
-import { uuid } from "~/server/validation";
 
 type GroupTree = {
   name: string;
   description: string;
-  startDate?: Date;
-  stopDate?: Date;
+  startDate?: string;
+  stopDate?: string;
   children?: GroupTree[];
-  parentId?: string;
 };
 
 const GroupSeed: Describe<GroupTree> = object({
   name: size(string(), 2, 50),
   description: size(string(), 2, 120),
-  startDate: optional(date()),
-  stopDate: optional(date()),
+  startDate: optional(dateString()),
+  stopDate: optional(dateString()),
   children: lazy(() => optional(array(GroupSeed))),
-  parentId: optional(uuid()),
 });
 
-const createGroup = async (
-  child: GroupTree,
-  parent?: Group,
-): Promise<Group | null> => {
-  assert(child, GroupSeed);
+const makeGroup = async (child: GroupTree, parent?: Group, level = 0) => {
+  const newGroup = await prisma.group.create({
+    data: {
+      name: child.name,
+      description: child.description,
+      startDate: new Date(child.startDate ?? parent?.startDate ?? "2000-01-01"),
+      stopDate: child.stopDate ? new Date(child.stopDate) : undefined,
+      parentId: parent?.id,
+    },
+  });
 
-  if (!child.startDate) {
-    child.startDate = parent?.startDate ?? new Date("2000-01-01");
-  }
-  if (parent) child.parentId = parent.id;
+  console.info(`${"...".repeat(level)}Created group: ${child.name}`);
 
-  const newGroup = await group.create(child as Prisma.GroupCreateInput);
-  console.info(`Created group: ${newGroup.name}.`);
-
-  if (child.children) {
+  if (child.children?.length) {
     for (const c of child.children) {
-      await createGroup(c, newGroup);
+      await makeGroup(c, newGroup, level + 1);
     }
   }
-
-  return newGroup;
 };
 
 export default async () => {
   assert(groups, array(GroupSeed));
 
-  for (const data of groups) {
-    const newGroup = await createGroup(data);
-    if (!newGroup) console.error(`Failed to create group with input: ${data}`);
-    else console.info(`Created group: ${newGroup.name}.`);
+  for (const group of groups) {
+    await makeGroup(group as unknown as GroupTree);
   }
 };
