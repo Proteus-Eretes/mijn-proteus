ed523354cd81639829e14e42fe1c5f7a8836052f
Gerben Mulder <gerbenmulder2508@gmail.com>
2022-11-16 17:59:56 +0100

    fix: 404 page breaking change

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -2,3 +2,7 @@
   <h1 class="text-6xl font-bold text-primary mb-4">404</h1>
   <p>Waar jij naar zocht hebben wij niet kunnen vinden..</p>
 </template>
+
+<script setup lang="ts">
+setResponseStatus(404);
+</script>
