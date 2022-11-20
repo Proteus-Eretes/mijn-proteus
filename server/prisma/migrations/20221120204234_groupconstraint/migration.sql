38a58dfcaa8e69fffdce1b0b10da19497c1711d1
Dany Sluijk <me@dany.dev>
2022-11-20 21:45:59 +0100

    feat(database): add study unique constraints

--- server/prisma/migrations/20221120204234_groupconstraint/migration.sql
+++ server/prisma/migrations/20221120204234_groupconstraint/migration.sql
@@ -3,6 +3,7 @@
 
   - You are about to drop the column `allowMembers` on the `Group` table. All the data in the column will be lost.
   - You are about to drop the column `allowSubgroups` on the `Group` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[name,level,institution]` on the table `Study` will be added. If there are existing duplicate values, this will fail.
   - Made the column `memberId` on table `Membership` required. This step will fail if there are existing NULL values in that column.
 
 */
@@ -16,5 +17,8 @@ DROP COLUMN "allowSubgroups";
 -- AlterTable
 ALTER TABLE "Membership" ALTER COLUMN "memberId" SET NOT NULL;
 
+-- CreateIndex
+CREATE UNIQUE INDEX "Study_name_level_institution_key" ON "Study"("name", "level", "institution");
+
 -- AddForeignKey
-ALTER TABLE "Membership" ADD CONSTRAINT "Membership_memberId_fkey" FOREIGN KEY ("memberId") REFERENCES "Member"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Membership" ADD CONSTRAINT "Membership_memberId_fkey" FOREIGN KEY ("memberId") REFERENCES "Member"("id") ON DELETE CASCADE ON UPDATE CASCADE;
