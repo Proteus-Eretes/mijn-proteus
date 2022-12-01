b0e8e11cbb6728f85af8f5c12f9b65779284b118
Dany Sluijk <me@dany.dev>
2022-12-02 00:37:39 +0100

    fix(server): don't assume the error received from seeding

--- server/plugins/seed.ts
+++ server/plugins/seed.ts
@@ -1,7 +1,6 @@
 import { reset, runSync } from "~/server/sync";
 import { seed } from "~/server/prisma/seed";
 import { member } from "~/server/logic";
-import { ApiError, ErrorCode } from "~/server/error";
 
 /**
  * Seed the database automatically when no members are detected.
@@ -23,10 +22,7 @@ export default defineNitroPlugin(async () => {
     await seed();
     await runSync();
   } catch (e) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const err: ApiError<ErrorCode> = e as any;
-
     console.error("Failed to seed the database!");
-    console.error(`[${err.code}] ${err.context}`);
+    console.error(e);
   }
 });
