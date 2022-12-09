3fcba66339ae96dbbe7168b8ac0a22cad90fa7c5
Dany Sluijk <me@dany.dev>
2022-12-09 18:25:50 +0100

    chore(member): add basic validation

--- server/api/members/[id]/index.patch.ts
+++ server/api/members/[id]/index.patch.ts
@@ -1,21 +1,9 @@
-import { enums, object, optional, size, string } from "superstruct";
-import { NameTitle } from "@prisma/client";
-
 import { getValidatedRouterParam, readValidatedBody } from "~/server/utils";
 import { member } from "~/server/logic";
-import { uuid } from "~~/server/validation/utils";
-
-const body = object({
-  title: optional(enums(Object.values(NameTitle))),
-  street: optional(size(string(), 1, 40)),
-  number: optional(size(string(), 1, 40)),
-  city: optional(size(string(), 1, 40)),
-  zipcode: optional(size(string(), 1, 20)),
-  country: optional(size(string(), 1, 40)),
-});
+import { MemberUpdateValidator, uuid } from "~~/server/validation";
 
 export default defineEventHandler(async (event) => {
   const id = await getValidatedRouterParam(event, "id", uuid());
-  const data = await readValidatedBody(event, body);
+  const data = await readValidatedBody(event, MemberUpdateValidator);
   return await member.update(id, data);
 });
--- server/validation/member.ts
+++ server/validation/member.ts
@@ -1,19 +1,27 @@
 import { NameTitle, Sex } from "@prisma/client";
 import {
-  defaulted,
+  assign,
   enums,
   Infer,
   object,
+  omit,
   optional,
+  partial,
   size,
   string,
 } from "superstruct";
 
 import { requiredContact } from "./contact";
-import { dateString } from "./utils";
+import { dateString, uuid } from "./utils";
 
-export const MemberCreateValidator = object({
-  title: defaulted(enums(Object.values(NameTitle)), NameTitle.NONE),
+/**
+ * Complete member validator.
+ * Validates all fields, but not the relations.
+ * This validator is not intended for direct use, but other validators should build upon this.
+ */
+const MemberValidator = object({
+  id: uuid(),
+  title: enums(Object.values(NameTitle)),
   initials: size(string(), 1, 10),
   firstName: size(string(), 1, 40),
   insertion: optional(size(string(), 1, 10)),
@@ -25,6 +33,22 @@ export const MemberCreateValidator = object({
   city: size(string(), 1, 40),
   zipcode: size(string(), 1, 20),
   country: size(string(), 1, 40),
-  contacts: requiredContact,
 });
+
+/**
+ * Member creation struct.
+ * It omits the ID of the member, and requires valid contacts to be added.
+ */
+export const MemberCreateValidator = assign(
+  omit(MemberValidator, ["id"]),
+  object({ contacts: requiredContact }),
+);
 export type MemberCreate = Infer<typeof MemberCreateValidator>;
+
+/**
+ * Member update validator.
+ * Simulair to member creation, but all fields are optional and does not allow for contacts to be updated.
+ * If the fields are actually allowed to be editted is not defined here.
+ */
+export const MemberUpdateValidator = partial(omit(MemberValidator, ["id"]));
+export type MemberUpdate = Infer<typeof MemberUpdateValidator>;
