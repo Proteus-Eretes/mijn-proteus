d25357072d2c62814ab2eeae948b733d340d5c73
Dany Sluijk <me@dany.dev>
2022-12-09 19:11:19 +0100

    chore(quotes): remove

--- server/api/quote/index.get.ts
+++
@@ -1,7 +0,0 @@
-import { Quote } from "@prisma/client";
-
-import { quote } from "~~/server/logic";
-
-export default defineEventHandler<Quote[]>(async () => {
-  return await quote.getAll();
-});
--- server/prisma/seed/index.ts
+++ server/prisma/seed/index.ts
@@ -1,6 +1,5 @@
 import materialType from "./materialType";
 import material from "./material";
-import quote from "./quote";
 import study from "./study";
 import group from "./group";
 import member from "./member";
@@ -8,7 +7,6 @@ import member from "./member";
 export const seed = async () => {
   await materialType();
   await material();
-  await quote();
   await study();
   await group();
   await member();
