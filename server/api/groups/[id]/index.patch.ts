fc30191f4b74f70984c9c45f71be403ae64752ec
Dany Sluijk <me@dany.dev>
2022-12-10 14:01:17 +0100

    feat(groups): add validation to updates

--- server/api/groups/[id]/index.patch.ts
+++ server/api/groups/[id]/index.patch.ts
@@ -1,18 +1,10 @@
-import { object, optional, size, string } from "superstruct";
-
-import { dateString, uuid } from "~~/server/validation";
 import { getValidatedRouterParam, readValidatedBody } from "~/server/utils";
+import { GroupUpdate, uuid } from "~~/server/validation";
 import { group } from "~/server/logic";
-
-const body = object({
-  name: optional(size(string(), 2, 50)),
-  description: optional(size(string(), 2, 120)),
-  startDate: optional(dateString()),
-  stopDate: optional(dateString()),
-});
 
 export default defineEventHandler(async (event) => {
   const id = await getValidatedRouterParam(event, "id", uuid());
-  const data = await readValidatedBody(event, body);
+  const data = await readValidatedBody(event, GroupUpdate);
+
   return await group.update(id, data);
 });
