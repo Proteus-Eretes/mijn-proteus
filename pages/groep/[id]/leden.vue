db3f03e3d6a1f189ed9e59e4adb6725c611daf36
Gerben Mulder <gerbenmulder2508@gmail.com>
2022-12-13 23:28:58 +0100

    feat(groups): add groups members page

---
+++ pages/groep/[id]/leden.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1 class="text-4xl text-primary font-bold mb-4">Leden Beheren</h1>
+  <div class="overflow-x-auto shadow">
+    <table class="table w-full">
+      <thead>
+        <tr>
+          <td>Naam</td>
+          <td>Functie</td>
+          <td>Sinds</td>
+          <td>Tot</td>
+          <td>Lid Type</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="member in group.members" :key="member.id">
+          <td>{{ member.id }}</td>
+          <td>{{ member.function }}</td>
+          <td>{{ member.startDate }}</td>
+          <td>{{ member.stopDate }}</td>
+          <td>{{ member.isAdmin ? "Beheerder" : "Lid" }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  group: unknown;
+}>();
+</script>
