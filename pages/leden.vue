24c62ea7e7ff0c725d57f9e8e41f406dae4ccbb9
Gerben Mulder <gerbenmulder2508@gmail.com>
2022-12-13 23:29:36 +0100

    feat(members): change members menu

--- pages/leden.vue
+++ pages/leden.vue
@@ -8,19 +8,16 @@
 </template>
 
 <script lang="ts" setup>
-const notification = undefined; // Als je hier een nummer invult, dan krijg je een notificatie met dat nummer
-
 const items = [
-  { title: "Zoeken", to: "/leden/zoeken" },
-  { title: "Mijn Profiel", to: "/leden/mijn-profiel" },
-  { title: "Administratie" },
-  { title: "Persoon toevoegen", to: "/leden/admin/toevoegen" },
-  { title: "Lidmaatschap toevoegen", to: "/leden/admin/lidmaatschappen" },
   {
-    title: "Uitschrijvingen",
-    to: "/leden/admin/uitschrijvingen",
-    badge: notification,
+    icon: "ic:search",
+    title: "Zoeken",
+    to: "/leden/zoeken",
   },
-  { title: "Tools", to: "/leden/admin/tools" },
+  {
+    icon: "ic:baseline-account-circle",
+    title: "Mijn Profiel",
+    to: "/leden/mijn-profiel",
+  },
 ];
 </script>
