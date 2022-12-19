6f03436be67dd2aa7d2c19dae56425f49cacee96
Gerben Mulder <gerbenmulder2508@gmail.com>
2022-12-19 16:35:41 +0100

    fix: consistent code style

--- server/api/groups/[id]/index.patch.ts
+++ server/api/groups/[id]/index.patch.ts
@@ -6,7 +6,7 @@ import { group } from "~/server/logic";
 export default defineEventHandler(async (event) => {
   const id = await getValidatedRouterParam(event, "id", uuid());
   const data = await readValidatedBody(event, GroupUpdate);
-
   const response = await group.update(id, data);
+
   return response ?? apiError(ErrorCode.NotFound, "Group not found");
 });
